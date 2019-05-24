09b1a535a6fcab8e270139fc37b9a2174ee06b40
kmhk.lu <kmhk.lu@aol.com>
2019-05-24 08:58:52 -0700

    - created project

---
+++ Weather/View/WeatherTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherTableViewCell.swift
+//  Weather
+//
+//  Created by com on 3/3/1398 AP.
+//  Copyright © 1398 KMHK. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
